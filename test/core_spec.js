c9cbf5338b54fe522e14e09deeacf0d7eff31509
Chloe Verity <chloeverity@hotmail.co.uk>
2019-01-31 16:11:16 +0000

    writing failing test to pair off entries for vote

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,7 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
 
-import {setEntries} from '../src/core';
+import {setEntries, next} from '../src/core';
 
 describe('Application logic', () => {
   describe('setEntries', () => {
@@ -21,5 +21,19 @@ describe('Application logic', () => {
         entries: List.of('Trainspotting', '28 Days Later')
       }))
     })
+  });
+  describe('next', () => {
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List.of('Sunshine')
+      }));
+    });
   });
 });
