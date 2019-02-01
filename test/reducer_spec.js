788189aee585c39ab8acb6ebfcfc87be9e2793aa
Chloe Verity <chloeverity@hotmail.co.uk>
2019-02-01 12:10:04 +0000

    writing failing test to check reducer can handle NEXT

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -13,4 +13,18 @@ describe('reducer', () => {
       entries: ['Trainspotting']
     }));
   });
+  it('handles NEXT', () => {
+    const initialState = fromJS({
+      entries: ['Trainspotting', '28 Days Later']
+    });
+    const action: {type: 'NEXT'};
+    const nextState: reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later']
+      },
+      entries: []
+    }));
+  });
 });
